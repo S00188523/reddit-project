7c9854eedb3c25ad96130c1d160e5f20718eb7db
Adam Grace <S00188523@atu.iem>
2023-02-26 13:09:13 +0000

    article.model.ts file added and exported

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
 
 @Component({
   selector: 'app-article',
@@ -6,26 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-  votes:number;
-  title:string;
-  link:string;
+  
+  article:Article
 
   constructor() {
-    this.title="Angular";
-    this.link="http://angular.io";
-    this.votes=10;
+    this.article=new Article("Angular", "http://angular.io", 10);
+    
    }
 
   ngOnInit() {
   }
 
   voteUp():boolean {
-    this.votes ++;
+    this.article.voteUp();
     return false;
   }
 
   voteDown():boolean {
-    this.votes --;
+    this.article.voteDown();
     return false;
   }
 
