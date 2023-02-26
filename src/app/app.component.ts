7a63b8d456f57f06cdd4a2a7db282ab4925d2aaa
Adam Grace <S00188523@atu.iem>
2023-02-26 20:30:41 +0000

    adding new articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  articles: Article[];
+
+  constructor(){
+    this.articles=[
+      new Article('Angular', 'http://angular.io', 5),
+      new Article('Google', 'http://google.ie', 3),
+      new Article('TikTok', 'http://tiktok.com', 1)
+    ];
+  }
   addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean{
+    this.articles.push(new Article(title.value, link.value,0));
+    title.value='';
+    link.value='';
+    
     console.log('Adding Article Title $(title.value) : and Adding Article Link $(link.value)');
     return false;
   }
